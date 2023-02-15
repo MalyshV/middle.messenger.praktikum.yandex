241016a92c88caded3896ee34656715eeb9e3e92
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2023-02-15 21:55:04 +0300

    feat: prepare for deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.use('/static', express.static(__dirname + '/dist'));
+app.use(express.static([__dirname, "dist"].join("/")));
 
 app.listen(PORT, function () {
   console.log(`App listening on port ${PORT}!`);
